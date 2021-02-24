00284c58eee7b597abb4ed0cd7cec5bea64def91
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-02-23 23:31:42 -0300

    Colocando title e favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,14 @@ import {Profile} from '../components/profile/Profile';
 import {CompletedChallenges} from '../components/completedChallenges/CompletedChallenges';
 import styles from '../styles/pages/Home.module.css';
 import {Countdown} from '../components/countdown/countdown';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
       <ExpirienceBar />
       <section>
         <div>
