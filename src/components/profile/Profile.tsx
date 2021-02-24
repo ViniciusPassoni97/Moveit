73482eb609bdd4365a7abb197067715864ef7a69
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-02-23 22:24:43 -0300

    Criando styles na pagina princial e componente profile

---
+++ src/components/profile/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+    return(
+        <div>
+            <img src='https://github.com/ViniciusPassoni97.png' alt='Image' />
+            <div>
+                <strong>Vinicius Passoni97</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import { ExpirienceBar } from "../components/expirienceBar/ExpirienceBar";
+import {Profile} from '../components/profile/Profile';
+import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <ExpirienceBar />
+      <section>
+        <div>
+          <Profile />
+        </div>
+        <div>
+          
+        </div>
+      </section>
     </div>
   )
 }
