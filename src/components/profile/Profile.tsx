d76a79cc67c6a5d1df1021545991d16ed8f8ca31
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-02-23 22:42:16 -0300

    Lidando com importações  de imagens publicas

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -6,7 +6,10 @@ export function Profile() {
             <img src='https://github.com/ViniciusPassoni97.png' alt='Image' />
             <div>
                 <strong>Vinicius Passoni97</strong>
-                <p>Level 1</p>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level 1
+                </p>
             </div>
         </div>
     );
