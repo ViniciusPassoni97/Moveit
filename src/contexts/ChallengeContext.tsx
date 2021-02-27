064b57b35556d9d6ba6fb8de0c49e4042beab25b
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-02-26 23:36:21 -0300

    Iniciando a criação de contextos

--- src/contexts/ChallengeContext.tsx
+++ src/contexts/ChallengeContext.tsx
@@ -1,3 +1,23 @@
-import {createContext} from 'react';
+import {createContext, useState, ReactNode} from 'react';
 
 export const ChallengeContext = createContext({});
+
+interface ChallengesProvieder{
+    children:ReactNode
+}
+
+export function ChallengesProvieder({children}:ChallengesProvieder) {
+    const [level,setLevel] = useState(1);
+    const [currentExpirience, setCurrentExpirence] = useState(0);
+    const [challengeCompleted,setChallengeCompleted] = useState(0);
+
+
+    function levelUp(){
+      setLevel(level+1);
+    }
+    return(
+    <ChallengeContext.Provider value={{level,currentExpirience,challengeCompleted,levelUp}}>
+        {children}
+    </ChallengeContext.Provider>
+    );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import '../styles/global.css';
-import {ChallengeContext} from '../contexts/ChallengeContext';
+import {ChallengeContext, ChallengesProvieder} from '../contexts/ChallengeContext';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const [level,setLevel] = useState(1);
+
+  function levelUp(){
+    setLevel(level+1);
+  }
   return(
-    <ChallengeContext.Provider value={'teste'}>
+    <ChallengesProvieder>
       <Component {...pageProps} />
-    </ChallengeContext.Provider>
+    </ChallengesProvieder>
   )
 }
 
