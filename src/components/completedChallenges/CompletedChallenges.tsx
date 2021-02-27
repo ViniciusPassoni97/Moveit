f9935b5df153493e0a59dfb10224abb7dcc1c3e2
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-02-27 19:19:03 -0300

    Adicionando challengeCompleted em src/components/CompletedChallenge.tsx

--- src/components/completedChallenges/CompletedChallenges.tsx
+++ src/components/completedChallenges/CompletedChallenges.tsx
@@ -1,9 +1,13 @@
+import { useContext } from 'react';
+import { ChallengeContext } from '../../contexts/ChallengeContext';
 import styles from '../../styles/components/CompletedChallenges.module.css';
 export function CompletedChallenges() {
+    const {challengeCompleted} = useContext(ChallengeContext);
+    
     return(
         <div className={styles.containerCompletedChallenges}>
             <span>Desafios completos</span>
-            <span>5</span>
+            <span>{challengeCompleted}</span>
         </div>
     );
 }
