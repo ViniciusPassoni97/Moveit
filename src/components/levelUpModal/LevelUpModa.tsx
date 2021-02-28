60daca06b442eec2936590b7db2a83e97d68efdb
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-02-28 15:54:59 -0300

    Terminando estrutura de HTML em src/components/LevelUpModal

--- src/components/levelUpModal/LevelUpModa.tsx
+++ src/components/levelUpModal/LevelUpModa.tsx
@@ -4,7 +4,12 @@ export function LevelUpModal() {
     return(
         <div className={styles.overlay}>
             <div className={styles.container}>
-            
+                <header>2</header>
+                <strong>Parabéns</strong>
+                <p>Você alcançou um novo level. </p>
+                <button type="button">
+                   <img src='/icons/close.svg' alt='Fechar Modal' /> 
+                </button>
             </div>
         </div>
     )
