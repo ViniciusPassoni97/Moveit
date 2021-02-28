a035e1e06a2856d15ffc075604822c20b0c2e7b6
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-02-28 15:51:12 -0300

    Criando LevelUpModal

---
+++ src/components/levelUpModal/LevelUpModa.tsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/components/LevelUpModal.module.css';
+
+export function LevelUpModal() {
+    return(
+        <div className={styles.overlay}>
+            <div className={styles.container}>
+            
+            </div>
+        </div>
+    )
+}
